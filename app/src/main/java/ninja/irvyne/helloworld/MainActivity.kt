a66b20065dcab0867fbb66f39ffd1e46e0146407
Thibaud BARDIN <thibaud.bardin@gmail.com>
2018-01-18 10:43:11 +0100

    Initial

---
+++ app/src/main/java/ninja/irvyne/helloworld/MainActivity.kt
@@ -0,0 +1,16 @@
+package ninja.irvyne.helloworld
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.longToast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener { longToast(textBlabla.text) }
+    }
+}
