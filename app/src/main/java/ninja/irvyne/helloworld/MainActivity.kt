4af4d2875d2581d9c1ba97a9bfffa83914cebc89
Thibaud BARDIN <thibaud.bardin@gmail.com>
2018-01-19 09:57:25 +0100

    update

--- app/src/main/java/ninja/irvyne/helloworld/MainActivity.kt
+++ app/src/main/java/ninja/irvyne/helloworld/MainActivity.kt
@@ -2,26 +2,38 @@ package ninja.irvyne.helloworld
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener {
-            supportFragmentManager
+            val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+            mapFragment.getMapAsync(this);
+
+            /*supportFragmentManager
                     .beginTransaction()
                     .add(R.id.fragment, WebViewFragment.newInstance(editText.text.toString()))
                     .commit()
 
-            /*startActivity(
+            startActivity(
                     Intent(this, WebViewActivity::class.java).apply {
                         putExtra(WebViewActivity.ARG_URL, editText.text.toString())
                     }
             )*/
         }
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        Log.d("MainActivity", "onMapReady")
     }
 }
